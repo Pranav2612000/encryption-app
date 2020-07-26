19243d5c2bf15a16711c2f476bc326f64b9ef1c7
Pranav <pranav2000joglekar@gmail.com>
2020-07-26 16:54:45 +0530

    feat: add button to navigate back to home on successful encryption/decryption

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './assets/LogoAndBrand.png';
 import './App.css';
-import {AppBar, Toolbar, ThemeProvider} from '@material-ui/core';
+import {AppBar, Toolbar, ThemeProvider, Button, Grid, Tabs, Tab} from '@material-ui/core';
 import createMuiTheme, { ThemeOptions, Theme } from '@material-ui/core/styles/createMuiTheme';
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 import MainContent from './Components/MainContent'
@@ -25,9 +25,19 @@ function App() {
       <div className="App">
         <ThemeProvider theme={theme}>
           <AppBar position = 'static' className="main-header" color='primary' >
-            <Toolbar>
-              <img src={logo} className="go" alt="logo" />
-            </Toolbar>
+            <Grid container className="header">
+              <Grid item xs={2}>
+                <Toolbar>
+                  <img src={logo} className="go" alt="logo" />
+                </Toolbar>
+              </Grid>
+              <Grid item xs={8}/>
+              <Grid item xs={2}>
+                <Button onClick={() => {window.location.reload()}}>
+                  Home
+                </Button>
+              </Grid>
+            </Grid>
           </AppBar>
         </ThemeProvider>
         <MainContent/>
